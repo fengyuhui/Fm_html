e28437121affd8fdae92d41af0360b2226d440ce
yuhui <497059671@qq.com>
2018-05-05 18:42:36 +0800

    子分类不规则测试通过

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ var subtypesList = [
     {subtypesId: '1', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '2', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '3', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '4', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },
     { subtypesId: '5', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '6', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '7', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '8', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },
     { subtypesId: '9', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '10', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '11', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '12', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },
-    { subtypesId: '13', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '14', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '15', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '16', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }
+    { subtypesId: '13', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '14', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '15', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '16', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },
+    { subtypesId: '13', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '14', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '15', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '16', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },{ subtypesId: '13', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },
+    { subtypesId: '13', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },{ subtypesId: '13', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }
 ]
 
 function onload() {
-
         adddiv();
 }
 
@@ -44,34 +45,10 @@ function adddiv() {  //循环添加子分类
         if(i%4 == 0 && i!=0){
             str += '</div>';
         }
-
-
-        // str += '<div class="types">' +
-        //     '<div class="stypes">' +
-        //     '<div data-id="1" class="energy">' +
-        //     '<image src="image/fm/music_icon.png" />' +
-        //     '<text>活力早晨</text>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '<div class="stypes">' +
-        //     '<div data-id="1" class="energy">' +
-        //     '<image src="image/fm/music_icon.png" />' +
-        //     '<text>活力早晨</text>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '<div class="stypes">' +
-        //     '<div data-id="1" class="energy">' +
-        //     '<image src="image/fm/music_icon.png" />' +
-        //     '<text>活力早晨</text>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '<div class="stypes">' +
-        //     '<div data-id="1" class="energy">' +
-        //     '<image src="image/fm/music_icon.png" />' +
-        //     '<text>活力早晨</text>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '</div>';
     }
     st.innerHTML = str;
+}
+
+function addType(){
+
 }
