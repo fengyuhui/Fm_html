183d83c567302b4d0ccbb8fa7ffe689e0bfe18e3
yuhui <497059671@qq.com>
2018-05-06 11:01:21 +0800

    分类选中样式变化

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ var sortingList = [{
 }];
 
 var showType = false;
+var sortingIndex = -1;
+var sortingId = -1;
+var subTypeId = -1;
+var subTypeIndex = -1;
 
 var subtypesList = [
     {subtypesId: '1', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '2', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '3', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' }, { subtypesId: '4', subtypeName: '活力早餐', subtypeIcon: 'image/fm/music_icon.png' },
@@ -24,6 +28,8 @@ var subtypesList = [
 ]
 
 function onload() {
+        var typeValue = document.getElementById('typeValue');
+        typeValue.innerHTML = sortingList[0].value;
         addTypes();
         addSubType();
 }
@@ -36,11 +42,12 @@ function addSubType() {  //循环添加子分类
     console.log("subtypesList.length"+subtypesList.length);
 
     for (var i = 1; i < subtypesList.length+1; i++) {
+        var j = i-1;
         if((i - 1)%4 == 0){
             str += '<div class="types">';
         }
         str += '<div class="stypes">' +
-            '<div data-id='+subtypesList[i-1].subtypesId+' class="energy" onclick="a()">' +
+            '<div data-id='+subtypesList[i-1].subtypesId+' data-index = '+j+' class="energy" onclick="choiceSub(this)">' +
             '<image src='+subtypesList[i-1].subtypeIcon+' />' +
             '<text>'+subtypesList[i-1].subtypeName+'</text>' +
             '</div>' +
@@ -58,8 +65,8 @@ function addTypes() {  //循环添加分类
     var str = '';
 
     str+='<div class="ensureitem">' +
-        '<text id = "cancel">取消</text>' +
-        '<text id = "ok" bindtap=\'ensure_type\'>确定</text>' +
+        '<text id = "cancel" onclick = "hideTypes()">取消</text>' +
+        '<text id = "ok" onclick = "hideTypes()">确定</text>' +
         '</div>'+
         '<div class="scroll" >' +
         '<div class="sorting-item">';
@@ -67,7 +74,7 @@ function addTypes() {  //循环添加分类
     console.log("sortingList.length"+sortingList.length);
 
     for (var i = 0; i < sortingList.length; i++) {
-        str+='<div class="backgrounddiv">' +
+        str+='<div class="backgrounddiv" onclick="selectSorting(this)" data-id='+sortingList[i].key+' data-index='+i+'>' +
             '<text decode="emsp">&emsp;'+sortingList[i].value+'&emsp;</text>' +
             '</div>';
     }
@@ -83,6 +90,33 @@ function showTypes(){ //显示分类选项列表
     document.getElementById('mask').style.display='block';
 }
 
-function a(){
-    alert("ere");
+function selectSorting(e){//选择分类并刷新数据
+    var id=e.getAttribute("data-id");
+    var index=e.getAttribute("data-index");
+
+    //先恢复先前选中的分类的样式
+    if(document.getElementsByClassName('activebackgrounddiv')[0]!=null){
+        document.getElementsByClassName('activebackgrounddiv')[0].setAttribute("class","backgrounddiv");
+    }
+
+    e.setAttribute("class","activebackgrounddiv");
+    sortingId = id;
+    sortingIndex = index;
+    var typeName = document.getElementById('typeValue');
+    typeName.innerHTML = sortingList[index].value;
+}
+
+function choiceSub(e){//选择子分类并刷新数据
+    var id=e.getAttribute("data-id");
+    var index=e.getAttribute("data-index");
+    subTypeId = id;
+    subTypeIndex = index;
+    var subTypeValue = document.getElementById('subTypeValue');
+    subTypeValue.innerHTML = subtypesList[index].subtypeName;
+}
+
+function hideTypes(){//隐藏分类选择项列表
+    showType = false;
+    document.getElementById('showTypes').style.display='none';
+    document.getElementById('mask').style.display='none';
 }
